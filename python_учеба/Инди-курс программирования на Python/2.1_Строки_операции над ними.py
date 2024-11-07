08058c237b1a666d54e6cb617313a6dfa7549cba
yurovskaya <yurovskaya@exportcenter.ru>
2024-11-07 15:01:02 +0300

    Решение задач Перевязь и про такси

---
+++ python_учеба/Инди-курс программирования на Python/2.1_Строки_операции над ними.py
@@ -0,0 +1,23 @@
+# Напишите программу, которая сначала считывает две фразы по очереди, а
+# потом воспроизводит их в той же последовательности, каждую на отдельной строке
+
+a = input()
+b = input()
+print(a,b, sep='\n')
+
+
+a = 'Ха'
+b = a + ' '
+print(b * 4)
+
+a = input()
+print(len(a))
+
+a = input()
+b = ord(a[0])
+print(b)
+c = ord(a[2])
+d = ord(a[4])
+print(f"Simvol code {a[0]} is {b}.")
+print(f"Simvol code {a[0]} is {c}.")
+print(f"Simvol code {a[0]} is {d}.")
